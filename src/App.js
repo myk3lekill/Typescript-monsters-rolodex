930ee8ef815a5dd64574173430a8744ad3c56b0d
me <me@gmail.com>
2024-01-06 11:30:04 +0100

    first commit: starting project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Functional Component (Hook)
+// function App() {
+  
+// }
+
+// Class Component 
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: { firstName: 'Myk3le', lastName: 'Zhang'},
+      company: 'ZTM'
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi {this.state.name.firstName} {this.state.name.lastName} from {this.state.company}
+          </p>
+          <button onClick={() => {
+            this.setState(() => {
+              return {
+                name:{firstName: 'Andrei', lastName: 'Niagoie'}
+              };
+             },
+            () => {
+                console.log(this.state)
+              })
+            }}>
+            Change Name
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
