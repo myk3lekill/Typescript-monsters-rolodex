c190ecd054ee933e3a7b514679a6d65d6a864c2f
me <me@gmail.com>
2024-01-10 11:08:51 +0100

    seventh commit: Code Optimization

--- src/App.js
+++ src/App.js
@@ -31,19 +31,24 @@ class App extends Component {
       ))
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    })}
+
   render() {
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this
+    
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
     
     return (
       <div className="App">
-      <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {
-        const searchField = event.target.value.toLocaleLowerCase();
-        this.setState(() => {
-          return { searchField };
-        })
-      }}/>
+      <input className='search-box' type='search' placeholder='search monsters' onChange={onSearchChange}/>
         {filteredMonsters.map((monster) => {
           return  <div key={monster.id}>
                   <h1>{monster.name}</h1>
