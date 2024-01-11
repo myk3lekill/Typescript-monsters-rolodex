57054727bc23375ca5d08084698fc5e800d5ac40
me <me@gmail.com>
2024-01-11 17:33:56 +0100

    tenth commit: SearchBox Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import CardList from './components/card-list/card-list.component';
 import './App.css';
 import { Component } from 'react';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 
 // Functional Component (Hook)
 // function App() {
@@ -48,7 +49,7 @@ class App extends Component {
     
     return (
       <div className="App">
-      <input className='search-box' type='search' placeholder='search monsters' onChange={onSearchChange}/>
+      <SearchBox className='search-box' placeholder='search mosters' onChangeHandler={onSearchChange}/>
         {/* {filteredMonsters.map((monster) => {
           return  <div key={monster.id}>
                   <h1>{monster.name}</h1>
