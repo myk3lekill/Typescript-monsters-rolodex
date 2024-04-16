2bfe45be6f6b65e1bddd7402515a598c8889edbc
me <me@gmail.com>
2024-04-16 17:35:56 +0200

    5th Commit: ChangeEvent Type From React

--- src/components/search-box/search-box.component.tsx
+++ src/components/search-box/search-box.component.tsx
@@ -1,10 +1,11 @@
-import { ChangeEventHandler } from 'react';
+import { ChangeEventHandler, ChangeEvent } from 'react';
 import'./search-box.styles.css';
 
 type SearchBoxProps = {
     className: string,
     placeholder?: string,
-    onChangeHandler: ChangeEventHandler<HTMLInputElement>
+    func: ChangeEventHandler
+    onChangeHandler: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
 const SearchBox = ({ className, placeholder, onChangeHandler }: SearchBoxProps) => {
